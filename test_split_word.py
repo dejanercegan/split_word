dc788f6533d8c9662c7db6f674c15498007ff035
Dejan Ercegan <48446054+dejanercegan@users.noreply.github.com>
2019-06-01 20:09:23 +0200

    Add files via upload

--- test_split_word.py
+++ test_split_word.py
@@ -5,5 +5,5 @@ def test_split():
 
     :return:
     """
-    wordsplit = split_word.split_word("test_split")
+    wordsplit = split_word.split_word("Dejan")
     assert (wordsplit == ['D', 'ej', 'an'])
